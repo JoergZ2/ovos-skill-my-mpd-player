222a2995f55ff9fb088364ecf48e8a011d17e782
JoergZ2 <joerg.zywek@gmx.de>
2025-02-06 08:54:49 +0100

    Update 2025-02-06
    Signed-off-by: JoergZ2 <joerg.zywek@gmx.de>

--- __init__.py
+++ __init__.py
@@ -2,9 +2,10 @@ import sys
 import os
 import time
 from ovos_workshop.skills import OVOSSkill
-from mycroft.skills import intent_handler
+from ovos_workshop.decorators import intent_handler
+#from mycroft.skills import intent_handler
 from adapt.intent import IntentBuilder
-from mycroft.util import extract_number
+#from mycroft.util import extract_number
 from ovos_backend_client.api import DeviceApi
 from ovos_bus_client.session import SessionManager
 from mpd import MPDClient
